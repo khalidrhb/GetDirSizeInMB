fd95d50be3a2cb23edce09ae7048db07ce08280b
khalidrhb <46495872+khalidrhb@users.noreply.github.com>
2021-02-19 10:48:49 +0530

    Get Directory Size in MB
    NOTE: This only works in .NET 4+.

---
+++ GetDirSizeInMB_Solution/Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Response.Write("Size: " + GetDirSizeInMB("Empty Folder") + " MB");
+        Response.Write("<br/> Size: " + GetDirSizeInMB("Products/ProductList1") + " MB");
+        Response.Write("<br/> Size: " + GetDirSizeInMB("Products") + " MB");
+    }
+
+
+    public string ServerRootDirPath = System.Web.HttpContext.Current.Server.MapPath("~/");
+    public float GetDirSizeInMB(string DirPath)
+    {
+        DirectoryInfo di = new DirectoryInfo(ServerRootDirPath + DirPath);
+        //Given size in bytes;
+        float FolderSize = (di.EnumerateFiles("*", SearchOption.AllDirectories).Sum(fi => fi.Length));
+        //Covert Size into MB and Return
+        return FolderSize = FolderSize / 1048576;  
+    }
+}
